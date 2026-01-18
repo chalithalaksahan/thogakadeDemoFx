34848d70e291c7f8e059fc23e7347814d017d5bf
Chalitha Laksahan <141056409+chalithalaksahan@users.noreply.github.com>
2026-01-18 12:12:41 +0530

    implement side menu buttons and there actions

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -11,14 +11,9 @@ public class Starter extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/customer_form.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/dashboard.fxml"))));
         stage.setTitle("Customer Form");
         stage.show();
-
-        Stage itemStage = new Stage();
-        itemStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/item_form.fxml"))));
-        itemStage.setTitle("Item Form");
-        itemStage.show();
 
     }
 }
---
+++ src/main/java/controller/DashboardController.java
@@ -0,0 +1,82 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+
+    @FXML
+    private AnchorPane dashRoot;
+
+    @FXML
+    void btnCustomerFormOnActiom(MouseEvent event) {
+        try {
+
+        URL resource = this.getClass().getResource("/view/customer_form.fxml");
+
+        assert resource != null;
+
+        Parent parent = null;
+
+            parent = FXMLLoader.load(resource);
+
+            dashRoot.getChildren().clear();
+            dashRoot.getChildren().add(parent);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @FXML
+    void btnOrderFormOnActiom(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnItemFormOnActiom(MouseEvent event) {
+        try {
+
+            URL resource = this.getClass().getResource("/view/item_form.fxml");
+
+            assert resource != null;
+
+            Parent parent = null;
+
+            parent = FXMLLoader.load(resource);
+
+            dashRoot.getChildren().clear();
+            dashRoot.getChildren().add(parent);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        try {
+
+            URL resource = this.getClass().getResource("/view/customer_form.fxml");
+
+            assert resource != null;
+
+            Parent parent = null;
+
+            parent = FXMLLoader.load(resource);
+
+            dashRoot.getChildren().clear();
+            dashRoot.getChildren().add(parent);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
