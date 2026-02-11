354159cafd85cd928eaf9dbc9d2a113c89076eb1
Chalitha Laksahan <141056409+chalithalaksahan@users.noreply.github.com>
2026-02-11 23:59:33 +0530

    customer service decouple done

--- src/main/java/repository/custom/impl/CustomerRepositoryImpl.java
+++ src/main/java/repository/custom/impl/CustomerRepositoryImpl.java
@@ -2,12 +2,12 @@ package repository.custom.impl;
 
 import db.DBConnection;
 import model.Customer;
+import model.tm.CustomerTM;
 import repository.custom.CustomerRepository;
 
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerRepositoryImpl implements CustomerRepository {
@@ -37,21 +37,118 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
     @Override
     public boolean update(Customer customer) {
-        return false;
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+
+            PreparedStatement psTm = connection.prepareStatement("update customer SET CustTitle=?,CustName=?,DOB=?,salary=?,CustAddress=?,City=?,Province=?,PostalCode=? WHERE CustID =?");
+
+            CustomerTM customerTM = new CustomerTM(
+                    customer.getId(),
+                    customer.getTitle(),
+                    customer.getName(),
+                    customer.getDob(),
+                    customer.getSalary(),
+                    customer.getAddress(),
+                    customer.getCity(),
+                    customer.getProvince(),
+                    customer.getPostalCode()
+            );
+
+            psTm.setString(1,customerTM.getTitle());
+            psTm.setString(2,customerTM.getFullName());
+            psTm.setString(3,customerTM.getDob().toString());
+            psTm.setDouble(4,customerTM.getSalary());
+            psTm.setString(5,customerTM.getAddress());
+            psTm.setString(6,customerTM.getCity());
+            psTm.setString(7,customerTM.getProvince());
+            psTm.setString(8,customerTM.getPostalCode());
+            psTm.setString(9,customerTM.getId());
+
+            return  psTm.executeUpdate()>0;
+
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public boolean deleteById(String id) {
-        return false;
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+
+            PreparedStatement psTm = connection.prepareStatement("DELETE FROM customer WHERE CustID = ?");
+            psTm.setString(1,id);
+
+            return psTm.executeUpdate()>0;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Customer getById(String id) {
-        return null;
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+
+            PreparedStatement psTm = connection.prepareStatement("SELECT * FROM customer WHERE CustID = ?");
+
+            psTm.setString(1,id);
+            ResultSet resultSet = psTm.executeQuery();
+
+            resultSet.next();
+
+
+            return new Customer(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getDate(4).toLocalDate(),
+                    resultSet.getDouble(5),
+                    resultSet.getString(6),
+                    resultSet.getString(7),
+                    resultSet.getString(8),
+                    resultSet.getString(9)
+            );
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<Customer> getAll() {
-        return List.of();
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            System.out.println(connection);
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Customer");
+
+            ArrayList<Customer> customerTMS = new ArrayList<>();
+
+            while (resultSet.next()){
+                Customer customer = new Customer(
+                        resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getDate(4).toLocalDate(),
+                        resultSet.getDouble(5),
+                        resultSet.getString(6),
+                        resultSet.getString(7),
+                        resultSet.getString(8),
+                        resultSet.getString(9)
+
+                );
+                customerTMS.add(customer);
+            }
+            return customerTMS;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
