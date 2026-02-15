6101501f2ab28edecb018abdd9e5a5d4ed9bf648
Chalitha Laksahan <141056409+chalithalaksahan@users.noreply.github.com>
2026-02-15 13:58:15 +0530

    remove unused code and comments from CrudUtil class

--- src/main/java/util/CrudUtil.java
+++ src/main/java/util/CrudUtil.java
@@ -19,3 +19,33 @@ public class CrudUtil {
         return (T)(Boolean)(psTm.executeUpdate()>0);
     }
 }
+
+
+//    executeupdate - insert,update delete ,
+//    executeQuary - select
+
+
+//    public static void main(String[] args) {
+//        Integer sum1 = getSum(10);
+//        Integer sum2 = getSum();
+//        Integer sum3 = getSum(10, 20, 30);
+//        Integer sum4 = getSum(10, 20, 30, 40, 50, 60, 70);
+//        Integer sum5 = getSum(10, 20, 30, 40, 50, 60, 70, 80, 90, 100);
+//
+//        System.out.println(sum1);
+//        System.out.println(sum2);
+//        System.out.println(sum3);
+//        System.out.println(sum4);
+//        System.out.println(sum5);
+//    }
+//
+//
+//    public static Integer getSum(Integer... args){
+//        int sum = 0;
+//
+//        for (int i = 0; i < args.length; i++) {
+//            sum += args[i];
+//        }
+//        return sum;
+//
+//    }
